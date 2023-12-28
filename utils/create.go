bc74d81d8f7ed416d48f382a35c33649ed226b95
Andres Court <andres@andrescourt.com>
2023-12-28 15:38:21 -0500

    Display a menu and select options

--- utils/create.go
+++ utils/create.go
@@ -1,12 +1,29 @@
 package utils
 
 import (
+	// "fmt"
 	"fmt"
+	"log"
+	"os/exec"
+	"strings"
 
 	"github.com/urfave/cli/v2"
 )
 
 func Create(c *cli.Context) error {
-	fmt.Printf("create a new branch called %s\n", c.String("name"))
-	return nil
+	err := createBranch(c.String("name"))
+	if err != nil {
+		if strings.Contains(err.Error(), "128") {
+			fmt.Println("Branch already exists")
+			return err
+		}
+		log.Fatal(err)
+	}
+	return err
+}
+
+func createBranch(name string) error {
+	cmd := exec.Command("git", "checkout", "-b", name)
+	err := cmd.Run()
+	return err
 }
--- utils/list.go
+++ utils/list.go
@@ -1,13 +1,69 @@
 package utils
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
 
+	// "github.com/lithammer/fuzzysearch/fuzzy"
+	"github.com/nexidian/gocliselect"
 	"github.com/urfave/cli/v2"
 )
 
 func List(c *cli.Context) error {
 	_ = c
-	fmt.Println("list all branches")
-	return nil
+	out, err := exec.Command("git", "branch", "-a").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b := strings.Split(string(out), "\n")
+	// branches := []string{}
+
+	menu := gocliselect.NewMenu("Select branch")
+	menu.AddItem("Cancel", "cancel")
+	menu.AddItem("Create new branch", "create")
+
+	for _, branch := range b {
+		if branch == "" {
+			continue
+		}
+		// branches = append(branches, branch)
+		menu.AddItem(branch, branch)
+	}
+
+	cmd := exec.Command("clear") // Linux example, its tested
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+	choice := menu.Display()
+
+	switch choice {
+	case "cancel":
+		return nil
+	case "create":
+		name := getBranchName()
+		err := createBranch(name)
+		if err != nil {
+			if strings.Contains(err.Error(), "128") {
+				fmt.Println("Branch already exists")
+				return err
+			}
+			log.Fatal(err)
+		}
+		return err
+	default:
+		fmt.Println(choice)
+		return nil
+	}
+}
+
+func getBranchName() string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter branch name: ")
+	text, _ := reader.ReadString('\n')
+
+	return strings.TrimSpace(text)
 }
